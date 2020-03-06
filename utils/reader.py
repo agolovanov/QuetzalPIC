25da1efe557495d1c8635ef96253a74b5e3b2551
Anton Golovanov <agolovanov256@gmail.com>
2020-03-06 18:36:34 +0300

    Add a python module to read output files

---
+++ utils/reader.py
@@ -0,0 +1,24 @@
+import h5py as _h5py
+import os as _os
+import numpy as _np
+
+class Simulation:
+    def __init__(self, path):
+        self.dir = path
+
+    def read_field(self, field_name, plane='xy'):
+        filename = _os.path.join(self.dir, f'Fields_{plane}.h5')
+        with _h5py.File(filename, 'r') as f:
+            field = f[field_name]
+            result = field[()].T
+
+            ny, nx = result.shape
+
+            x0, y0 = field.attrs['origin']
+            dx, dy = field.attrs['steps']
+
+            x = _np.linspace(x0, x0 + (nx - 1) * dx, nx)
+            y = _np.linspace(y0, y0 + (ny - 1) * dy, ny)
+            
+            xx, yy = _np.meshgrid(x, y)
+            return xx, yy, result
