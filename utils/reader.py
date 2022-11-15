378b0f8a94446b22b16efcb9004776fffb5617c1
Anton Golovanov <agolovanov256@gmail.com>
2022-11-15 12:10:35 +0200

    Add ability to read fields without creating a mesh

--- utils/reader.py
+++ utils/reader.py
@@ -12,19 +12,22 @@ class Simulation:
         else:
             raise ValueError(f"Path [{path}] is not a directory")
 
-    def read_field(self, field_name, iteration=0, plane='xy'):
+    def read_field(self, field_name, iteration=0, plane='xy', include_mesh=False):
         filename = _os.path.join(self.dir, f'Fields_{plane}_{iteration:03d}.h5')
         with _h5py.File(filename, 'r') as f:
             field = f[field_name]
             result = field[()].T
 
-            ny, nx = result.shape
+            if include_mesh:
+                ny, nx = result.shape
 
-            x0, y0 = field.attrs['origin']
-            dx, dy = field.attrs['steps']
+                x0, y0 = field.attrs['origin']
+                dx, dy = field.attrs['steps']
 
-            x = _np.linspace(x0, x0 + (nx - 1) * dx, nx)
-            y = _np.linspace(y0, y0 + (ny - 1) * dy, ny)
-            
-            xx, yy = _np.meshgrid(x, y)
-            return xx, yy, result
+                x = _np.linspace(x0, x0 + (nx - 1) * dx, nx)
+                y = _np.linspace(y0, y0 + (ny - 1) * dy, ny)
+                
+                xx, yy = _np.meshgrid(x, y)
+                return xx, yy, result
+            else:
+                return result
